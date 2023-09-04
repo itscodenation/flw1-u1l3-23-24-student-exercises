7cacde737e629fcbfee1f7482fa8f96c9dc73f36
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-04 17:08:42 -0400

    Added solo coding practice

---
+++ magicalCreature.js
@@ -0,0 +1,39 @@
+// Welcome to the magical zoo! Your task is to create and manage a list of magical creatures. Let's have some fun!
+
+// 1. Declare a variable named 'creatureName' and assign it a magical creature's name as a string.
+
+
+
+// 2. Declare a variable named 'creatureDescription' and describe its magical properties as a string.
+
+
+// 3. Do you believe that this creature is friendly to humans? 
+// Declare a variable named 'isFriendly' and assign it a boolean value.
+
+
+
+// 4. The magical zoo has an age restriction! Declare a variable named 'visitorAge' and assign it a random age between 5 and 60.
+// Hint: Use Math.random() for generating random numbers.
+
+
+
+// 5. Determine if the visitor is allowed to see the creature.
+// Declare a variable named 'canVisit' and assign it an expression to see 
+// if the visitorAge variable is greater than 15.
+// Hint: use this resource if you need support: https://www.w3schools.com/js/js_booleans.asp 
+
+
+
+// 6. Combine all the information into a summary.
+// Declare a variable named 'zooSummary' and combine all the above information using concatenation.
+
+
+// 7. Print the summary using console.log()
+
+
+
+// Challenge:
+// 8. Now, imagine there are three more creatures in the zoo. 
+//    Repeat Tasks 1-6 for each creature, then combine all summaries into a single variable 'allZooSummaries'.
+// 9. Print the 'allZooSummaries' to see the details of all the creatures in the magical zoo.
+
