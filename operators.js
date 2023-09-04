9f54fd22d83119f8ad69cfdb3cdbc63a56cc37d5
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-04 16:22:24 -0400

    Added operators coding exercise

---
+++ operators.js
@@ -0,0 +1,28 @@
+// Arithmetic Operations
+
+
+// 1. Addition
+// Create two variables, num1 and num2, and assign them any two numbers of your choice.
+// Add the two numbers and store the result in a new variable called sum.
+// Print the result using console.log().
+
+
+
+// 2. Subtraction
+// Subtract num2 from num1 and store the result in a new variable called difference.
+// Print the result.
+
+
+// 3. Multiplication
+// Multiply num1 by num2 and store the result in a new variable called product.
+// Print the result.
+
+
+
+// String Operations
+
+// 4. Concatenation with +
+// Create two string variables, str1 and str2, and assign them any two strings of your choice.
+// Concatenate (combine) the two strings using the + operator and store the result in a new variable called combinedString.
+// Print the result.
+
---
+++ variables.js
@@ -0,0 +1,14 @@
+// 1. Create a variable called `favoriteColor` and assign it a string value of your favorite color.
+let favoriteColor = "YOUR_COLOR_HERE";
+
+// 2. Create a variable called `favoriteFood` and assign it a string value of your favorite food.
+
+
+// 3. Create a variable called `favoriteNumber` and assign it a number value of your favorite number.
+
+
+// 4. Create a variable called `isSunnyToday` and assign it a boolean value based on if it's sunny today.
+
+
+// 5. Console log each variable on a new line.
+
